101ac3c6b3e1ffe8ff291956812065fea8d027d1
Danuta Huletskaya <huletskaya.danuta@student.ehu.lt>
2025-01-20 00:19:45 +0300

    feat(removeItems): implement deep comparison logic

--- src/removeItems.js
+++ src/removeItems.js
@@ -6,8 +6,38 @@
  */
 module.exports.removeItems = function removeItems(arr, value) {
   let newArr = [];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== value) newArr.push(arr[i]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== value) {
+      if (typeof arr[i] === 'object' && typeof value === 'object') {
+        let objectEqual = true;
+
+        for (let key in value) {
+          if (arr[i][key] !== value[key]) {
+            objectEqual = false;
+            break;
+          }
+        }
+
+        if (Array.isArray(arr[i]) && Array.isArray(value)) {
+          if (arr[i].length !== value.length) {
+            objectEqual = false;
+          } else {
+            for (let j = 0; j < arr[i].length; j++) {
+              if (arr[i][j] !== value[j]) {
+                objectEqual = false;
+                break;
+              }
+            }
+          }
+        }
+
+        if (!objectEqual) {
+          newArr.push(arr[i]);
+        }
+      } else {
+        newArr.push(arr[i]);
+      }
+    }
   }
   return newArr;
 };
